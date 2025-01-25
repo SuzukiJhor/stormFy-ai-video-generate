0ffacec12e1c8f39d362c41a170a14054ad97cbb
jhordan <jhordan.suzuki@lmcorp.local>
2025-01-25 03:39:59 -0300

    feat: add text area to select custom

---
+++ app/dashboard/create/-components/SelectTopic.jsx
@@ -0,0 +1,35 @@
+'use client'
+import React from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+import { Textarea } from '@/components/ui/textarea';
+
+export default function SelectTopic() {
+    const [ selectedOption, setSelectedOption ] = React.useState();
+    const options = [ 'Prompt Customizado', 'História Aleatória IA', 'História de Medo']
+  return (
+    <div>
+        <h2 className='font-bold text-2xl text-primary'>Conteudo</h2>
+        <p className='text-gray-500'>Qual o Tópico do seu Video ?</p>
+        <Select onValueChange={(value)=> setSelectedOption(value)}>
+            <SelectTrigger className="w-full mt-02 p-6 text-lg">
+                <SelectValue placeholder="Tipo de Conteudo" />
+            </SelectTrigger>
+            <SelectContent>
+                {options.map((item, index)=>(
+                <SelectItem value={item}>{item}</SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+
+        {selectedOption === 'Prompt Customizado' && 
+            <Textarea className='mt-03' placeholder='Escreva seu Prompt Aqui'/>
+        }
+    </div>
+  )
+}
--- app/dashboard/create/page.jsx
+++ app/dashboard/create/page.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import SelectTopic from './-components/SelectTopic'
 
 export default function CreateNew() {
   return (
-    <div>
-        <h2> + Criar mais um</h2>
+    <div className='md:px-20'>
+      <h2 className='font-bold text-4xl text-primary text-center'> + Criar mais um</h2>
+      <div className='mt-10 shadow-md p-10'>
+        {/* Topico */}
+        <SelectTopic />
+      </div>
     </div>
   )
 }
