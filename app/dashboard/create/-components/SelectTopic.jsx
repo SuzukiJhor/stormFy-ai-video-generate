24e8385d975b01c5f3a9a49e59ed73672f9de97c
jhordan <jhordan.suzuki@lmcorp.local>
2025-01-25 12:54:14 -0300

    feat: add child component to selectTopic

--- app/dashboard/create/-components/SelectTopic.jsx
+++ app/dashboard/create/-components/SelectTopic.jsx
@@ -9,14 +9,17 @@ import {
   } from "@/components/ui/select"
 import { Textarea } from '@/components/ui/textarea';
 
-export default function SelectTopic() {
+export default function SelectTopic({onUserSelect}) {
     const [ selectedOption, setSelectedOption ] = React.useState();
     const options = [ 'Prompt Customizado', 'História Aleatória IA', 'História de Medo']
   return (
     <div>
         <h2 className='font-bold text-2xl text-primary'>Conteudo</h2>
         <p className='text-gray-500'>Qual o Tópico do seu Video ?</p>
-        <Select onValueChange={(value)=> setSelectedOption(value)}>
+        <Select onValueChange={(value) => {
+          setSelectedOption(value)
+          value != 'Prompt Customizado' && onUserSelect('topic', value)
+          }}>
             <SelectTrigger className="w-full mt-02 p-6 text-lg">
                 <SelectValue placeholder="Tipo de Conteudo" />
             </SelectTrigger>
@@ -28,7 +31,11 @@ export default function SelectTopic() {
         </Select>
 
         {selectedOption === 'Prompt Customizado' && 
-            <Textarea className='mt-03' placeholder='Escreva seu Prompt Aqui'/>
+            <Textarea 
+              className='mt-03' 
+              placeholder='Escreva seu Prompt Aqui'
+              onChange={(e)=> onUserSelect('Topic', e.target.value)}
+            />
         }
     </div>
   )
--- app/dashboard/create/page.jsx
+++ app/dashboard/create/page.jsx
@@ -1,13 +1,18 @@
+'use client'
 import React from 'react'
 import SelectTopic from './-components/SelectTopic'
 
 export default function CreateNew() {
+  const [ formData, seFormData ] = React.useState([]);
+  const onHandleInputChange = (fieldName, fieldValue) => {
+    console.log(fieldName, fieldValue)
+  }
   return (
     <div className='md:px-20'>
       <h2 className='font-bold text-4xl text-primary text-center'> + Criar mais um</h2>
       <div className='mt-10 shadow-md p-10'>
         {/* Topico */}
-        <SelectTopic />
+        <SelectTopic onUserSelect={onHandleInputChange}/>
       </div>
     </div>
   )
