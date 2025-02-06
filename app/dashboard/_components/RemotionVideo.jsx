de68a8712f21a70a82b59661f2d618f059d616c5
jhordan <jhordan.suzuki@lmcorp.local>
2025-02-05 23:15:52 -0300

    feat: add animate into video created

--- app/dashboard/_components/RemotionVideo.jsx
+++ app/dashboard/_components/RemotionVideo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AbsoluteFill, Audio, Img, Sequence, useCurrentFrame, useVideoConfig } from 'remotion'
+import { AbsoluteFill, Audio, Img, interpolate, Sequence, useCurrentFrame, useVideoConfig } from 'remotion'
 
 export default function RemotionVideo({ audioScript, captions, imageUrl, videoScript, setDurationFrame }) {
   const { fps } = useVideoConfig();
@@ -12,28 +12,39 @@ export default function RemotionVideo({ audioScript, captions, imageUrl, videoSc
     const currentTime = frame / 30 * 1000 //convert fram to milesseconds
     const currentCaption = captions.find((item) => currentTime >= item.start && currentTime <= item.end)
     return currentCaption ? currentCaption?.word : '';
-  }  
+  }
   return (
     <AbsoluteFill key={captions.length}>
-      {imageUrl?.map((item, index) => (
-        <>
-          <Sequence key={index} from={(index * defineDurationFrame()) / imageUrl?.length} durationInFrames={defineDurationFrame()}>
-            <Img
-              src={item}
-              style={{
-                width: '100%',
-                height: '100%',
-                objectFit: 'cover'
-              }}
-            />
-            <AbsoluteFill className='text-black flex font-bold
-              justify-start items-center
+      {imageUrl?.map((item, index) => {
+        const startTime = (index * defineDurationFrame()) / imageUrl?.length;
+        const duration = defineDurationFrame();
+        const scale = (index) => interpolate(
+          frame,
+          [startTime, startTime + duration / 2, startTime + duration],
+          index % 2 == 0 ? [1, 1.8, 1] : [1.8, 1, 1.8],
+          { extrapolateLeft: 'clamp', extrapolateRight: 'clamp' }
+        )
+        return (
+          <>
+            <Sequence key={index} from={startTime} durationInFrames={duration}>
+              <Img
+                src={item}
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                  transform: `scale(${scale(index)})`
+                }}
+              />
+              <AbsoluteFill className='text-white flex font-bold 
+                justify-start items-center
               '>
-              <h2 className='text-2xl'>{defineCurrentCaptions()}</h2>
-            </AbsoluteFill>
-          </Sequence>
-        </>
-      ))}
+                <h2 className='text-2xl'>{defineCurrentCaptions()}</h2>
+              </AbsoluteFill>
+            </Sequence>
+          </>
+        )
+      })}
       <Audio src={audioScript} />
     </AbsoluteFill>
   )
