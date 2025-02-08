7d5b9d11347886c77649afc3a52d2c833ed41ae5
jhordan <jhordan.suzuki@lmcorp.local>
2025-02-08 10:23:51 -0300

    fix: improve

--- drizzle.config.js
+++ drizzle.config.js
@@ -7,6 +7,6 @@ export default defineConfig ({
     dialect: 'postgresql', // 'mysql' | 'sqlite' | 'turso'
     schema: './configs/schema.js',
     dbCredentials: {
-      url: 'postgresql://neondb_owner:npg_pvnai3gzbPJ9@ep-raspy-credit-a88vl7pa-pooler.eastus2.azure.neon.tech/stormfy-ai-video-generator?sslmode=require',
+      url: process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL
     }
   })
